bcba6d70e2a5672a2f014b52ede46ceb8380ccfa
Hasbem <marvinmsb69@gmail.com>
2024-05-09 14:09:11 +0200

    ajustement search bar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -4,18 +4,22 @@ import "./SearchBar.css";
 function SearchBar() {
   const [searchTerm, setSearchTerm] = useState("");
 
-
-
   return (
-    <div className="search-bar-container">
-      <input
-        type="text"
-        placeholder="Recherche ton héros Marvel"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        className="search-bar-input"
-      />
-      <img src="./image/search.png" alt="Rechercher" className="search-bar-button"/>
+    <div className="search-bar">
+      <div className="search-bar-container">
+        <input
+          type="text"
+          placeholder="Recherche ton héros Marvel"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="search-bar-input"
+        />
+        <img
+          src="./image/search.png"
+          alt="Rechercher"
+          className="search-bar-button"
+        />
+      </div>
     </div>
   );
 }
